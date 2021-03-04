5dded67991fdd42c0d398c70d84f8a5ffe33cd95
David Cassal Studio <davidcassal@gmail.com>
2021-03-04 13:39:07 -0300

    Navegação no mobile
    Melhorias de navegação na versão mobile

--- js/toggleMenu.js
+++ js/toggleMenu.js
@@ -1,6 +1,13 @@
 function menuToggle(){
     var nav = document.getElementById('full-menu');
     var toggle = document.getElementById('toggle');
-    nav.classList.toggle("active")
-    toggle.classList.toggle("active")
+    nav.classList.toggle("active");
+    toggle.classList.toggle("active");
+}
+
+function menuClosed(){
+    var nav = document.getElementById('full-menu');
+    var closeBar = document.getElementById('close-bar');
+    nav.classList.toggle("active");
+    closeBar.classList.toggle("closed");
 }
