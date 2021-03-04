08a54d0ca0071b221df7141f63f849cf9eab2310
David Cassal Studio <davidcassal@gmail.com>
2021-03-04 12:13:26 -0300

    Navegação
    Menu em todas as páginas (falta estilizar na versão mobile) e melhoria do botão de voltar em todas as páginas

---
+++ js/toggleMenu.js
@@ -0,0 +1,6 @@
+function menuToggle(){
+    var nav = document.getElementById('full-menu');
+    var toggle = document.getElementById('toggle');
+    nav.classList.toggle("active")
+    toggle.classList.toggle("active")
+}
